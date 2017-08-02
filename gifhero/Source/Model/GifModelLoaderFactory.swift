e8dcfd8b054769512568fd5bfaa6c0388b3855a1
vauxhall <zero.griffin@gmail.com>
2017-08-02 05:55:26 -0500

    Create factory queue

--- gifhero/Source/Model/GifModelLoaderFactory.swift
+++ gifhero/Source/Model/GifModelLoaderFactory.swift
@@ -2,5 +2,17 @@ import Foundation
 
 extension GifModelLoader
 {
+    private static let kQueueLabel:String = "iturbide.gifhero"
     
+    class func factoryQueue() -> DispatchQueue
+    {
+        let queue = DispatchQueue(
+            label:kQueueLabel,
+            qos:DispatchQoS.background,
+            attributes:DispatchQueue.Attributes(),
+            autoreleaseFrequency:DispatchQueue.AutoreleaseFrequency.inherit,
+            target:DispatchQueue.global(qos:DispatchQoS.QoSClass.background))
+        
+        return queue
+    }
 }
