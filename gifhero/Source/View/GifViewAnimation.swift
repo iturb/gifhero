dfd7f7efd205054edf6cc8f926b6d9be50b02eac
vauxhall <zero.griffin@gmail.com>
2017-08-02 04:37:32 -0500

    Add functions to call strategy

--- gifhero/Source/View/GifViewAnimation.swift
+++ gifhero/Source/View/GifViewAnimation.swift
@@ -2,5 +2,18 @@ import Foundation
 
 extension GifView
 {
+    func preLoad()
+    {
+        strategy?.preLoad()
+    }
     
+    func startAnimation()
+    {
+        strategy?.startAnimating()
+    }
+    
+    func stopAnimation()
+    {
+        strategy?.stopAnimating()
+    }
 }
