37c0b34add326b0472f64b24ececce231f59eda4
vauxhall <zero.griffin@gmail.com>
2017-08-02 16:40:12 -0500

    Implement size for view

--- gifhero/Source/Model/GifModelRenderModeBetterPerformance.swift
+++ gifhero/Source/Model/GifModelRenderModeBetterPerformance.swift
@@ -2,7 +2,35 @@ import UIKit
 
 class GifModelRenderModeBetterPerformance:GifModelRenderModeProtocol
 {
+    private let kSizeMultiplier:CGFloat = 0.5
+    private let kMinSize:CGFloat = 10
+    
     required init()
     {
+    }
+    
+    func gifSizeForView(view:UIView) -> CGSize
+    {
+        let viewSize:CGSize = view.bounds.size
+        let viewWidth:CGFloat = viewSize.width
+        let viewHeight:CGFloat = viewSize.height
+        var scaledWidth:CGFloat = viewWidth * kSizeMultiplier
+        var scaledHeight:CGFloat = viewHeight * kSizeMultiplier
+        
+        if scaledWidth < kMinSize
+        {
+            scaledWidth = kMinSize
+        }
+        
+        if scaledHeight < kMinSize
+        {
+            scaledHeight = kMinSize
+        }
+        
+        let gifSize:CGSize = CGSize(
+            width:scaledWidth,
+            height:scaledHeight)
+        
+        return gifSize
     }
 }
