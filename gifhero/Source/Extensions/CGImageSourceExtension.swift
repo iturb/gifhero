7695df9dc5bf479a47a590c0d41058bff0679fa9
vauxhall <zero.griffin@gmail.com>
2017-08-02 06:17:15 -0500

    Factory source

--- gifhero/Source/Extensions/CGImageSourceExtension.swift
+++ gifhero/Source/Extensions/CGImageSourceExtension.swift
@@ -1,6 +1,63 @@
+import Foundation
 import ImageIO
 
 extension CGImageSource
 {
+    class func factorySource(url:URL) -> CGImageSource?
+    {
+        guard
+            
+            let data:CFData = loadData(url:url)
+            
+        else
+        {
+            return nil
+        }
+        
+        let options:CFDictionary = sourceNoCacheOptions()
+        
+        guard
+            
+            let source:CGImageSource = CGImageSourceCreateWithData(
+                data,
+                options)
+            
+        else
+        {
+            return nil
+        }
+        
+        return source
+    }
     
+    //MARK: private
+    
+    private class func loadData(url:URL) -> CFData?
+    {
+        let data:Data
+        
+        do
+        {
+            try data = Data(
+                contentsOf:url,
+                options:Data.ReadingOptions.uncached)
+        }
+        catch
+        {
+            return nil
+        }
+        
+        let cfData:CFData = data as CFData
+        
+        return cfData
+    }
+    
+    private class func sourceNoCacheOptions() -> CFDictionary
+    {
+        let dictionary:[String:Any] = [
+            kCGImageSourceShouldCache as String:kCFBooleanFalse]
+        let cfDictionary:CFDictionary = dictionary as CFDictionary
+        
+        return cfDictionary
+    }
 }
