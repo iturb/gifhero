ac824fe5c60574407e2c9709d2932fe5bb61617e
vauxhall <zero.griffin@gmail.com>
2017-08-02 05:34:43 -0500

    Add needs display on main thread

--- gifhero/Source/View/GifViewDraw.swift
+++ gifhero/Source/View/GifViewDraw.swift
@@ -2,6 +2,18 @@ import UIKit
 
 extension GifView
 {
+    override func draw(_ rect:CGRect)
+    {
+        
+    }
     
+    func asyncNeedsDisplay()
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.setNeedsDisplay()
+        }
+    }
 }
 
