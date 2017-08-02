3b0ca5bd990707bbd4945284514d9639cd029b6d
vauxhall <zero.griffin@gmail.com>
2017-08-02 04:54:26 -0500

    Factory strategies

--- gifhero/Source/View/GifViewStrategy.swift
+++ gifhero/Source/View/GifViewStrategy.swift
@@ -6,4 +6,28 @@ extension GifView
     {
         strategy = GifStrategyStand(view:self)
     }
+    
+    func strategyLoadAndAnimate()
+    {
+        strategy = GifStrategyLoad(
+            view:self,
+            animateWhenReady:true)
+    }
+    
+    func strategyLoadAndPause()
+    {
+        strategy = GifStrategyLoad(
+            view:self,
+            animateWhenReady:false)
+    }
+    
+    func strategyAnimate()
+    {
+        strategy = GifStrategyAnimate(view:self)
+    }
+    
+    func strategyPause()
+    {
+        strategy = GifStrategyPause(view:self)
+    }
 }
