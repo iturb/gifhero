1168664fe6337828c78e5f8b1a314cdaa9a5dcae
vauxhall <zero.griffin@gmail.com>
2017-08-02 02:16:47 -0500

    Create app delegate

---
+++ gifhero/Demo/Controller/CAppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate:UIResponder, UIApplicationDelegate
+{
+    var window:UIWindow?
+    
+    func application(_ application:UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey:Any]?) -> Bool
+    {
+        let window:UIWindow = UIWindow(frame:UIScreen.main.bounds)
+        window.backgroundColor = UIColor.white
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+}
