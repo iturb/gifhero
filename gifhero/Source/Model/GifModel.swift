55f3df89a1f1a4b1e70eb86e494dc1455dde9115
vauxhall <zero.griffin@gmail.com>
2017-08-02 18:33:34 -0500

    Add init to model

--- gifhero/Source/Model/GifModel.swift
+++ gifhero/Source/Model/GifModel.swift
@@ -2,5 +2,14 @@ import Foundation
 
 class GifModel
 {
+    let frames:[GifModelFrame]
+    let count:Int
+    var index:Int
     
+    init(frames:[GifModelFrame])
+    {
+        self.frames = frames
+        count = frames.count
+        index = 0
+    }
 }
