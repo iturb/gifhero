289b61b997b910d505b646d967141dd63355b192
vauxhall <zero.griffin@gmail.com>
2017-08-02 02:19:01 -0500

    Rename CAppDelegate to AppDelegate

--- gifhero/Demo/Controller/AppDelegate.swift
+++ gifhero/Demo/Controller/AppDelegate.swift
@@ -5,7 +5,9 @@ class AppDelegate:UIResponder, UIApplicationDelegate
 {
     var window:UIWindow?
     
-    func application(_ application:UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey:Any]?) -> Bool
+    func application(
+        _ application:UIApplication,
+        didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey:Any]?) -> Bool
     {
         let window:UIWindow = UIWindow(frame:UIScreen.main.bounds)
         window.backgroundColor = UIColor.white
