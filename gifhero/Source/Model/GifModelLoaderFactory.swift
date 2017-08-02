d02e24a8603af96ac3acaeef87908b14c6aacc72
vauxhall <zero.griffin@gmail.com>
2017-08-02 16:27:09 -0500

    Factory render mode router

--- gifhero/Source/Model/GifModelLoaderFactory.swift
+++ gifhero/Source/Model/GifModelLoaderFactory.swift
@@ -37,6 +37,24 @@ extension GifModelLoader
         return gifUrl
     }
     
+    class func factoryRenderModeRouter() -> [
+        GifView.RenderMode:
+        GifModelRenderModeProtocol.Type]
+    {
+        let router:[
+            GifView.RenderMode:
+            GifModelRenderModeProtocol.Type] = [
+                GifView.RenderMode.betterQuality:
+                    GifModelRenderModeBetterQuality.self,
+                GifView.RenderMode.betterPerformance:
+                    GifModelRenderModeBetterPerformance.self,
+                GifView.RenderMode.half:
+                    GifModelRenderModeHalf.self
+            ]
+        
+        return router
+    }
+    
     //MARK: private
     
     private class func factoryUrlFrom(gifNamed:String) -> URL?
