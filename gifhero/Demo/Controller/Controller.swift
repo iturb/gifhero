d24943c955549575bbd694300642211d2179b6d1
vauxhall <zero.griffin@gmail.com>
2017-08-02 02:53:48 -0500

    Make controller collection delegate

--- gifhero/Demo/Controller/Controller.swift
+++ gifhero/Demo/Controller/Controller.swift
@@ -1,10 +1,22 @@
 import UIKit
 
-class Controller:UIViewController
+class Controller:
+    UIViewController,
+    UICollectionViewDelegate,
+    UICollectionViewDataSource,
+    UICollectionViewDelegateFlowLayout
 {
+    private let kReusableIdentifier:String = "viewCell"
+    
     override func loadView()
     {
         let view:View = View()
+        view.collectionView.delegate = self
+        view.collectionView.dataSource = self
+        view.collectionView.register(
+            ViewCell.self,
+            forCellWithReuseIdentifier:
+            kReusableIdentifier)
         self.view = view
     }
 }
