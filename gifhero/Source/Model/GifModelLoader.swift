84cb81b114efed29713df9b004acfa66b766cfe1
vauxhall <zero.griffin@gmail.com>
2017-08-02 06:21:37 -0500

    Add source in factory

--- gifhero/Source/Model/GifModelLoader.swift
+++ gifhero/Source/Model/GifModelLoader.swift
@@ -1,4 +1,5 @@
-import Foundation
+import UIKit
+import ImageIO
 
 class GifModelLoader
 {
@@ -14,10 +15,75 @@ class GifModelLoader
         {
             gifUrl = url
         }
+        else if let gifNamed:String = view.gifNamed
+        {
+            gifUrl = GifModelLoader.factoryUrl(
+                gifNamed:gifNamed)
+        }
         else
         {
-            
+            gifUrl = nil
         }
+        
+        guard
+        
+            let url:URL = gifUrl
+        
+        else
+        {
+            strategy?.loadFail()
+            
+            return
+        }
+        
+        loadWithUrl(url:url)
+    }
+    
+    private func loadWithUrl(url:URL)
+    {
+        guard
+        
+            let source:CGImageSource = CGImageSource.factorySource(url:url)
+        
+        else
+        {
+            strategy?.loadFail()
+            
+            return
+        }
+    }
+    
+    private func loadFrames(source:CGImageSource)
+    {
+        let count:Int = CGImageSourceGetCount(source)
+        let options:CFDictionary = VGif.frameOptions()
+        var frames:[VGifFrame] = []
+        
+        for index:Int in 0 ..< count
+        {
+            guard
+                
+                let image:CGImage = VGif.frameImage(
+                    source:source,
+                    index:index,
+                    options:options)
+                
+                else
+            {
+                continue
+            }
+            
+            let itemDuration:TimeInterval =  frameDuration(
+                source:source,
+                index:index)
+            
+            let frame:VGifFrame = VGifFrame(
+                image:image,
+                duration:itemDuration)
+            frames.append(frame)
+        }
+        
+        framesLoaded(frames:frames)
     }
     
     //MARK: public
