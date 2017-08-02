29bdfdaf4d60c1b684bc641f707549bd8dad67ee
vauxhall <zero.griffin@gmail.com>
2017-08-02 02:50:33 -0500

    Add collection to view

--- gifhero/Demo/View/View.swift
+++ gifhero/Demo/View/View.swift
@@ -3,10 +3,40 @@ import UIKit
 class View:UIView
 {
     private(set) weak var collectionView:UICollectionView!
+    private let kCollectionInterItemSpace:CGFloat = 10
+    private let kCollectionMarginVertical:CGFloat = 30
     
     init()
     {
         super.init(frame:CGRect.zero)
+        backgroundColor = UIColor.white
+        
+        let screenBounds:CGRect = UIScreen.main.bounds
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = kCollectionInterItemSpace
+        flow.minimumInteritemSpacing = kCollectionInterItemSpace
+        flow.sectionInset = UIEdgeInsets(
+            top:kCollectionMarginVertical,
+            left:0,
+            bottom:kCollectionMarginVertical,
+            right:0)
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:screenBounds,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceVertical = true
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
     }
     
     required init?(coder:NSCoder)
