9d821d3bd0ca3a9c05a335460a922ac5ab70f204
vauxhall <zero.griffin@gmail.com>
2017-08-02 17:43:36 -0500

    Implement factory gif

--- gifhero/Demo/Model/ModelName.swift
+++ gifhero/Demo/Model/ModelName.swift
@@ -2,5 +2,13 @@ import Foundation
 
 class ModelName:ModelProtocol
 {
+    private let kGifName:String = "assetPigIceCream.gif"
     
+    func factoryGif() -> GifView
+    {
+        let view:GifView = GifView()
+        view.gifNamed = kGifName
+        
+        return view
+    }
 }
