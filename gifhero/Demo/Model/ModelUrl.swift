103402ebe9e4aa055454da26cc6afb1211116e9b
vauxhall <zero.griffin@gmail.com>
2017-08-02 17:45:24 -0500

    Implement factory

--- gifhero/Demo/Model/ModelUrl.swift
+++ gifhero/Demo/Model/ModelUrl.swift
@@ -2,5 +2,17 @@ import Foundation
 
 class ModelUrl:ModelProtocol
 {
+    private let kGifName:String = "assetPigJump.gif"
     
+    func factoryGif() -> GifView
+    {
+        let url:URL? = Bundle.main.url(
+            forResource:kGifName,
+            withExtension:nil)
+        
+        let view:GifView = GifView()
+        view.url = url
+        
+        return view
+    }
 }
