bb7a667b0ad425a36f4fbe872b4fb0d3b3d1c18a
vauxhall <zero.griffin@gmail.com>
2017-08-02 16:57:32 -0500

    Implement fitting rect

--- gifhero/Source/Extensions/CGImageExtension.swift
+++ gifhero/Source/Extensions/CGImageExtension.swift
@@ -6,8 +6,11 @@ extension CGImage
         targetSize:CGSize,
         contentMode:UIViewContentMode) -> CGImage?
     {
-        let imageRect:CGRect = rectToDraw(
-            atSize:targetSize,
+        let width:CGFloat = CGFloat(self.width)
+        let height:CGFloat = CGFloat(self.height)
+        let imageRect:CGRect = targetSize.fittingRectFor(
+            width:width,
+            height:height,
             contentMode:contentMode)
         
         let newImage:CGImage? = resizeToFit(
@@ -54,63 +57,5 @@ extension CGImage
         UIGraphicsEndImageContext()
         
         return newImage
-    }
-    
-    func rectToDraw(
-        atSize:CGSize,
-        contentMode:UIViewContentMode) -> CGRect
-    {
-        let imageWidth:CGFloat = CGFloat(width)
-        let imageHeight:CGFloat = CGFloat(height)
-        let targetWidth:CGFloat = atSize.width
-        let targetHeight:CGFloat = atSize.height
-        let ratioWidth:CGFloat = imageWidth / targetWidth
-        let ratioHeight:CGFloat = imageHeight / targetHeight
-        
-        let ratio:CGFloat = ratioFor(
-            contentMode:contentMode,
-            ratioA:ratioWidth,
-            ratioB:ratioHeight)
-        
-        let scaledWidth:CGFloat = imageWidth / ratio
-        let scaledHeight:CGFloat = imageHeight / ratio
-        let remainWidth:CGFloat = targetWidth - scaledWidth
-        let remainHeight:CGFloat = targetHeight - scaledHeight
-        let marginX:CGFloat = remainWidth / 2.0
-        let marginY:CGFloat = remainHeight / 2.0
-        let drawingRect:CGRect = CGRect(
-            x:marginX,
-            y:marginY,
-            width:scaledWidth,
-            height:scaledHeight)
-        
-        return drawingRect
-    }
-    
-    //MARK: private
-    
-    private func ratioFor(
-        contentMode:UIViewContentMode,
-        ratioA:CGFloat,
-        ratioB:CGFloat) -> CGFloat
-    {
-        let ratio:CGFloat
-        
-        switch contentMode
-        {
-        case UIViewContentMode.scaleAspectFit:
-            
-            ratio = max(ratioA, ratioB)
-            
-            break
-            
-        default:
-            
-            ratio = min(ratioA, ratioB)
-            
-            break
-        }
-        
-        return ratio
     }
 }
