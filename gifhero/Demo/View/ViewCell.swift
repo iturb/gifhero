fd76c93aec90afaf1877b7e45da3164b23871bce
vauxhall <zero.griffin@gmail.com>
2017-08-02 17:39:40 -0500

    Implement protocol in view

--- gifhero/Demo/View/ViewCell.swift
+++ gifhero/Demo/View/ViewCell.swift
@@ -2,10 +2,30 @@ import UIKit
 
 class ViewCell:UICollectionViewCell
 {
+    private weak var gifView:GifView?
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
     //MARK: public
     
     func config(model:ModelProtocol)
     {
+        self.gifView?.removeFromSuperview()
         
+        let gifView:GifView = model.factoryGif()
+        gifView.frame = bounds
+        self.gifView = gifView
+        
+        addSubview(gifView)
     }
 }
