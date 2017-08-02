7c48d561b877a3a89d262a1ff24dd26b9343e2c4
vauxhall <zero.griffin@gmail.com>
2017-08-02 04:53:00 -0500

    Create init for strategy load

--- gifhero/Source/Strategy/GifStrategyLoad.swift
+++ gifhero/Source/Strategy/GifStrategyLoad.swift
@@ -2,5 +2,12 @@ import Foundation
 
 class GifStrategyLoad:GifStrategy
 {
+    private var animateWhenReady:Bool
     
+    init(view:GifView, animateWhenReady:Bool)
+    {
+        self.animateWhenReady = animateWhenReady
+        
+        super.init(view:view)
+    }
 }
