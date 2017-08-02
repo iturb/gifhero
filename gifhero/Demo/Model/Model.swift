34e46a092a99b05df193b1fe1332f52fe9fc369c
vauxhall <zero.griffin@gmail.com>
2017-08-02 17:46:23 -0500

    Add items to factory

--- gifhero/Demo/Model/Model.swift
+++ gifhero/Demo/Model/Model.swift
@@ -6,8 +6,12 @@ class Model
     
     private class func factoryItems() -> [ModelProtocol]
     {
+        let itemUrl:ModelUrl = ModelUrl()
+        let itemName:ModelName = ModelName()
+        
         let items:[ModelProtocol] = [
-        ]
+            itemUrl,
+            itemName]
         
         return items
     }
