8596b3025263ab2b550cd91ecad1d02f1c338a3e
vauxhall <zero.griffin@gmail.com>
2017-08-03 08:10:46 -0500

    Create strategy loaded

---
+++ gifhero/Source/Strategy/GifStrategyLoaded.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+class GifStrategyLoaded:GifStrategy
+{
+    override init(view:GifView)
+    {
+        super.init(view:view)
+        
+        view.displayLink?.isPaused = false
+    }
+    
+    override func changeSource()
+    {
+        super.changeSource()
+        
+        view?.strategyLoadAndAnimate()
+    }
+    
+    override func changeSize()
+    {
+        super.changeSize()
+        
+        view?.strategyLoadAndAnimate()
+    }
+    
+    override func stopAnimating()
+    {
+        super.stopAnimating()
+        
+        view?.strategyPause()
+    }
+}
